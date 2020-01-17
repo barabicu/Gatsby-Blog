3a375b0b68bfc1bf01d6b57089b4eba43290fca6
nemo <nemo@nemonoMBP.lan>
2020-01-17 17:22:38 +0900

    Add post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+// importを追記
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  // queryの取得処理を追加
+  const data = useStaticQuery(graphql`
+    query allContentfulBlogPost {
+      allContentfulBlogPost {
+        nodes {
+          id
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Title lists</h1>
+      {/* 取得したデータを表示する処理を追加 */}
+      {data.allContentfulBlogPost.nodes.map(({ id, title }) => (
+        <h2 key={id}>
+          {title}
+        </h2>
+      ))}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
